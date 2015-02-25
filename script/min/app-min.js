f87623e12af4a3de45692e3a473d207e094abe00
Chrysto Panayotov <burnandbass@gmail.com>
2015-02-25 17:35:06 +0200

    Added some functionality to the site. Should refactor the js

---
+++ script/min/app-min.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+
+	$('li').on('click', function(){
+		$('li').each(function(){
+			if($(this).hasClass('active')) {
+				$(this).removeClass('active');
+			}
+		});
+
+		$(this).toggleClass('active');
+	});
+
+
+	// refactor!!!!
+	$('.jokes-nav').on('click', function(){
+		$('article').each(function(){
+			if($(this).hasClass('visible')) {
+				$(this).removeClass('visible')
+			}
+		});
+		$('.jokes').toggleClass('visible');
+	});
+
+	$('.home-nav').on('click', function(){
+		$('article').each(function(){
+			if($(this).hasClass('visible')) {
+				$(this).removeClass('visible')
+			}
+		});
+		$('.home').toggleClass('visible');
+	});
+	$('.gifs-nav').on('click', function(){
+		$('article').each(function(){
+			if($(this).hasClass('visible')) {
+				$(this).removeClass('visible')
+			}
+		});
+		$('.gifs').toggleClass('visible');
+	});
+	$('.pics-nav').on('click', function(){
+		$('article').each(function(){
+			if($(this).hasClass('visible')) {
+				$(this).removeClass('visible')
+			}
+		});
+		$('.pics').toggleClass('visible');
+	});
+
+});
+
